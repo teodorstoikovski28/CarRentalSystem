479b8c619194111e74f706f0e2fc5b08c08c5062
19teodor13 <tedilioni@gmail.com>
2026-06-04 11:21:18 +0300

    Added Categories CRUD

--- CarRentalSystem/Controllers/CategoriesController.cs
+++ CarRentalSystem/Controllers/CategoriesController.cs
@@ -1,12 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarRentalSystem.Data;
+using CarRentalSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarRentalSystem.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
+            var categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            context.Categories.Update(category);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            context.Categories.Remove(category);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
